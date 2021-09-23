807c838548547ba6c233e95e00c98dd02345823a
Goodbyez <miradadesigual@gmail.com>
2021-09-23 20:04:03 +0200

    Update register DONE

--- src/containers/Management/management.jsx
+++ src/containers/Management/management.jsx
@@ -6,17 +6,27 @@ import axios from 'axios';
 const Management = () => {
 
     const [search, setSearch] = useState("");
-    const [newMovie, setNewMovie] = useState({title:'',director:'',year:'',genre:'',actors:''});
+    const [addMovie, setAddMovie] = useState({title:'',director:'',year:'',genre:'',actors:''});
     const [movies, setMovies] = useState([]);
     const [allowAdd, setAllowAdd] = useState('');
+    const [updMovie, setUpdMovie] = useState('');
     const [filteredMovies, setFilteredMovies] = useState([]);
+    const [updateData, setUpdateData] = useState({id:'',title:'',director:'',year:'',genre:'',actors:''});
+   
 
     useEffect(()=>{
         allMovies();
     },[]);
 
+    useEffect(()=>{
+        setFilteredMovies(movies);
+    },[movies]);
+
     const dataMovie = (e) => {
-        setNewMovie({...newMovie, [e.target.name]: e.target.value});
+        setAddMovie({...addMovie, [e.target.name]: e.target.value});
+    }
+    const updDataMovie = (e) => {
+        setUpdateData({...updateData, [e.target.name]: e.target.value});
     }
 
     // useEffect(() => {
@@ -27,28 +37,35 @@ const Management = () => {
     //     );
     // }, [search, movies]);
 
+    // SHOW ALL MOVIES AND SAVE ON HOOKS
     const allMovies = async () => {
         try{
             let res = await axios.get('http://127.0.0.1:8000/api/allmovies');
-            console.log(res.data.data);
+
             setMovies(res.data.data);
+            setFilteredMovies(res.data.data);
         } catch (err) {
             console.log({message: err.message})
         }
     }
 
-    const newReg = async () => {
+    // CREATE NEW MOVIE
+    const newMovie = async () => {
 
         try {
             let body = {
-                title: newMovie.title,
-                director: newMovie.director,
-                year: newMovie.year,
-                genre: newMovie.genre,
-                actors: newMovie.actors,
+                title: addMovie.title,
+                director: addMovie.director,
+                year: addMovie.year,
+                genre: addMovie.genre,
+                actors: addMovie.actors,
             }
+            
+
             let res = await axios.post(`http://127.0.0.1:8000/api/createmovie`, body)
+
             if (res){
+                setMovies(res.data.data);
                 setAllowAdd(false);
             }
         }
@@ -57,7 +74,51 @@ const Management = () => {
         }   
     }
 
-    const stringSearch = (e) => {
+    // UPDATE MOVIE
+    const updateMovie = (data) => {
+        setUpdMovie(true);
+        setUpdateData(data);
+    }
+
+    const upMovie = async () => {
+        try {
+            let body = {
+                movie_id: updateData.id,
+                title: updateData.title,
+                director: updateData.director,
+                year: updateData.year,
+                genre: updateData.genre,
+                actors: updateData.actors,
+            }
+            console.log(body)
+            let res = await axios.post(`http://127.0.0.1:8000/api/updatemovie`, body)
+
+            if (res){
+                setMovies(res.data.data);
+                setUpdMovie(false);
+            }
+        }
+        catch (err) {
+            console.log({message: err.message});
+        }  
+    }
+
+    // DELETE MOVIE
+    const deleteMovie = async (id) => {
+        try {
+            let body = {
+                movie_id: id,
+            }
+
+            let res = await axios.post(`http://127.0.0.1:8000/api/deletemovie`, body)
+
+            if (res){
+                setMovies(res.data.data);
+            }
+        }
+        catch (err) {
+            console.log({message: err.message});
+        }   
 
     }
 
@@ -84,20 +145,22 @@ const Management = () => {
 
     return (
         <div id="containerManager">
+            
             <div className="optionsBar">
                 {/* <input className="textSearch" type="string" name="title" placeholder="title" onChange={stringSearch}></input> */}
                 <div className="button" onClick={()=>setAllowAdd(true)}>NEW</div>
             </div>
+
             {allowAdd && (
                 <div className="data addNewMovie">
-                    <input onFocus={(e) => e.target.value=''} className="inputsNewMovie dataTitle" type="text" name="title" onChange={dataMovie} placeholder="Title"></input>
-                    <input onFocus={(e) => e.target.value=''} className="inputsNewMovie dataDirector" type="text" name="director" onChange={dataMovie} placeholder="Director"></input>
-                    <input onFocus={(e) => e.target.value=''} className="inputsNewMovie dataYear" type="number" name="year" onChange={dataMovie} placeholder="Year"></input>
-                    <input onFocus={(e) => e.target.value=''} className="inputsNewMovie dataGenre" type="text" name="genre" onChange={dataMovie} placeholder="Genre"></input>
-                    <input onFocus={(e) => e.target.value=''} className="inputsNewMovie dataActors" type="text" name="actors" onChange={dataMovie} placeholder="Actors"></input>
+                    <input className="inputsNewMovie dataTitle" type="text" name="title" onChange={dataMovie} placeholder="Title"></input>
+                    <input className="inputsNewMovie dataDirector" type="text" name="director" onChange={dataMovie} placeholder="Director"></input>
+                    <input className="inputsNewMovie dataYear" type="number" name="year" onChange={dataMovie} placeholder="Year"></input>
+                    <input className="inputsNewMovie dataGenre" type="text" name="genre" onChange={dataMovie} placeholder="Genre"></input>
+                    <input className="inputsNewMovie dataActors" type="text" name="actors" onChange={dataMovie} placeholder="Actors"></input>
                     
                     <div className="minibuttonBox">
-                        <div className="minibutton" onClick={()=>newReg()}>ADD</div>
+                        <div className="minibutton" onClick={()=>newMovie()}>ADD</div>
                         <div className="minibutton" onClick={()=>setAllowAdd(false)}>X</div>
                     </div>
                 </div>
@@ -113,19 +176,38 @@ const Management = () => {
                     <div className="infoData dataActors">ACTORS</div>
                 </div>
 
-                {movies.map((movie, index)=>(
-                    <div className="movieRow">
-                        <div className="data" key={index}>
+                {filteredMovies.map((movie, index)=>(
+                    <div className="movieRow" key={index}>
+                        <div className="data" >
                             <div className="infoData dataTitle">{movie.title}</div>
                             <div className="infoData dataDirector">{movie.director}</div>
                             <div className="infoData dataYear">{movie.year}</div>
-                            <div className="infoData dataGenre">{movie.genre.split(',').join(', ')}</div>
-                            <div className="infoData dataActors">{movie.actors.split(',').join(', ')}</div> 
+                            <div className="infoData dataGenre">{movie.genre}</div>
+                            <div className="infoData dataActors">{movie.actors}</div> 
+                        </div>
+                        <div className="minibuttonBox">
+                            <div className="minibutton" onClick={()=>updateMovie(movie)}>UPD</div>
+                            <div className="minibutton" onClick={()=>deleteMovie(movie.id)}>X</div>
                         </div>
                     </div>
                 ))}
 
             </div>
+
+            {updMovie && (
+                <div className="data addNewMovie red">
+                    <input className="inputsNewMovie dataTitle" type="text" name="title" onChange={updDataMovie} placeholder={updateData.title}></input>
+                    <input className="inputsNewMovie dataDirector" type="text" name="director" onChange={updDataMovie} placeholder={updateData.director}></input>
+                    <input className="inputsNewMovie dataYear" type="number" name="year" onChange={updDataMovie} placeholder={updateData.year}></input>
+                    <input className="inputsNewMovie dataGenre" type="text" name="genre" onChange={updDataMovie} placeholder={updateData.genre}></input>
+                    <input className="inputsNewMovie dataActors" type="text" name="actors" onChange={updDataMovie} placeholder={updateData.actors}></input>
+                    
+                    <div className="minibuttonBox">
+                        <div className="minibutton" onClick={()=>upMovie()}>ACCEPT</div>
+                        <div className="minibutton" onClick={()=>setUpdMovie(false)}>X</div>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
